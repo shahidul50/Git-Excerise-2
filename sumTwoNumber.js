ba092a66f92670988c79d679a97bf71c6ff88177
Shahidul <shahid.cse.19@gmail.com>
2023-01-29 13:11:21 +0600

    I change the function with rest operator. Then we sum multipule number

--- sumTwoNumber.js
+++ sumTwoNumber.js
@@ -1,5 +1,8 @@
-const sumTowNumber = (a,b) => {
-       return a+b
+const sumTowNumber = (...value) => {
+      let sum = 0
+      for(let num of value)
+             sum += num
+    return sum
 }
 
-console.log(sumTowNumber(5,10));
+console.log(sumTowNumber(5,10,12,13));
